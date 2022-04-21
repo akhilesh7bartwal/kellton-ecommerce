29c58e9adc635b36b4f8bf3058ccfd9103310f29
akhilesh7bartwal <akhileshbartwal61@gmail.com>
2022-04-21 17:38:50 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# KELLTON ECOMMERECE
+Clothing Website with the following features:
+ -- Shop Products
+ -- Add to Cart
+ -- Sign up with google
+ -- Sign in
+ -- Checkout
+ -- 
---
+++ src/component/menu-item/menu-item.component.jsx
@@ -0,0 +1,23 @@
+import './menu-item.style.scss'
+const MenuItem= ({menu}) => {
+    return (
+        <>
+                <div className='menu-item'>
+                    <div
+                        className='background-image'
+                        style={{
+                            backgroundImage: `url(${menu.imageUrl})`
+                        }                          
+                        }
+                    />
+                    <div className='content'>
+                        <div className='title'>{menu.title}</div>
+                        <div className='subtitle'>SHOP NOW</div>
+                    </div>
+                </div>
+
+        </>
+    )
+}
+
+export default MenuItem
